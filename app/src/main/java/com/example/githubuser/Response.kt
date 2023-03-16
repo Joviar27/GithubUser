1c7261b7d01e58b2ded7c154516e2bf901f49f9b
Joviar27 <joviar27@gmail.com>
2023-03-16 08:31:48 +0700

    Create Pojo generated json response

---
+++ app/src/main/java/com/example/githubuser/Response.kt
@@ -0,0 +1,49 @@
+package com.example.githubuser
+
+import com.google.gson.annotations.SerializedName
+
+data class UserListResponse(
+
+	@field:SerializedName("items")
+	val items: List<User>
+)
+
+data class User(
+
+	@field:SerializedName("login")
+	val login: String,
+
+	@field:SerializedName("avatar_url")
+	val avatarUrl: String,
+
+	@field:SerializedName("id")
+	val id: Int,
+
+)
+
+data class DetailUserResponse(
+
+	@field:SerializedName("following_url")
+	val followingUrl: String,
+
+	@field:SerializedName("login")
+	val login: String,
+
+	@field:SerializedName("followers_url")
+	val followersUrl: String,
+
+	@field:SerializedName("followers")
+	val followers: Int,
+
+	@field:SerializedName("avatar_url")
+	val avatarUrl: String,
+
+	@field:SerializedName("following")
+	val following: Int,
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("location")
+	val location: String,
+)
