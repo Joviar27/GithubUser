5a6c72e733f6daf78123368f9652bb52f3c4f80c
Joviar27 <joviar27@gmail.com>
2023-04-03 16:21:27 +0700

    Create Database

---
+++ app/src/main/java/com/example/githubuser/data/local/room/UserDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.githubuser.data.local.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.githubuser.data.local.entity.BookmarkedUserEntity
+import com.example.githubuser.data.local.entity.FollowEntity
+import com.example.githubuser.data.local.entity.UserEntity
+
+@Database(entities = [UserEntity::class, BookmarkedUserEntity::class, FollowEntity::class], version = 1, exportSchema = false)
+abstract class UserDatabase : RoomDatabase() {
+    abstract fun userDao():UserDao
+    abstract fun bookmarkedUserDao():BookmarkedUserDao
+    abstract fun followDao() : FollowDao
+
+    companion object {
+        @Volatile
+        private var instance: UserDatabase? = null
+        fun getInstance(context: Context): UserDatabase =
+            instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    UserDatabase::class.java, "User.db"
+                ).build()
+            }
+    }
+}
