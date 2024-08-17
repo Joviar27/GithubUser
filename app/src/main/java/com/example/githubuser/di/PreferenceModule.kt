947be42bea6a8ebed204285ac1cb404c6981245c
Joviar <joviar27@gmail.com>
2024-08-18 06:13:23 +0700

    fix: Module annotation on preference module

--- app/src/main/java/com/example/githubuser/di/PreferenceModule.kt
+++ app/src/main/java/com/example/githubuser/di/PreferenceModule.kt
@@ -2,12 +2,14 @@ package com.example.githubuser.di
 
 import android.content.Context
 import com.example.githubuser.data.local.preference.ThemePreference
+import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+@Module
 @InstallIn(SingletonComponent::class)
 object PreferenceModule {
 
